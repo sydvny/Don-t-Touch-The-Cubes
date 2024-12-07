d07fe4a643d9b70e2517aa6af5966df36c3de611
Sydney <sydneygertrudemarkoff@gmail.com>
2024-12-07 11:40:52 -0500

    first commit

---
+++ Assets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+    {
+        [Header("Destruction Timer")]
+        public float timeToDestruction;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        Invoke("DestroyObject", timeToDestruction);
+        }
+
+        // Update is called once per frame
+  
+        void DestroyObject()
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+
+
---
+++ Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    [Header("Spawn Cube Object")]
+    public GameObject spawnCube;
+    [Header("Default Difficulty")]
+    public float difficulty = 40f;
+    float spawn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawn = difficulty * Time.deltaTime;
+        difficulty = Time.deltaTime * 4f;
+        
+        while (spawn > 0)
+        {
+            spawn -= 1;
+            Vector3 v3Pos = transform.position + new Vector3(Random.value * 40f - 20f, 0, Random.value *40f - 20f);
+            Quaternion qRotation = Quaternion.Euler(0, Random.value * 360f, Random.value * 30f);
+            GameObject createObject = Instantiate(spawnCube, v3Pos, qRotation);
+        }
+    }
+}
+
+//STEP 19
